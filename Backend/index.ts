0087463bbb1b82a35a56c2db195d8da62eff9281
Ammar Meshaal <157367715+Ammarrs@users.noreply.github.com>
2025-07-22 20:36:08 +0300

    some updates

--- Backend/index.ts
+++ Backend/index.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response } from "express";
+import tasksRoute from './src/router/tasks'
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -6,9 +7,7 @@ const app = express();
 const host = process.env.HOST;
 const port = process.env.PORT;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send(`Test the app on port: ${port}`);
-});
+app.use('/tasks', tasksRoute);
 
 app.listen(port, () => {
   console.log(`server running on http://${host}:${port}/`);
