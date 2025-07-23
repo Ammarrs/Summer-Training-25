65d485fb7ffe87a83ca9d7125b3029d71a1f271f
Ammar Meshaal <157367715+Ammarrs@users.noreply.github.com>
2025-07-23 14:25:59 +0300

    making route

---
+++ Backend/src/routes/auth.ts
@@ -0,0 +1,7 @@
+import express, { Request, Response } from 'express';
+import { register } from '../modules/auth';
+const router = express.Router();
+
+router.post("/register", register);
+
+export default router;
